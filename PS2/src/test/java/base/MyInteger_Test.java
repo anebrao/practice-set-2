d0044b7e083519a44d60331ba6ffb9aed3680719
alexianebrao <alexianebrao@wifi-roaming-128-4-82-226.host.udel.edu>
2016-09-22 23:36:29 -0400

    class diagram

--- PS2/src/test/java/base/MyInteger_Test.java
+++ PS2/src/test/java/base/MyInteger_Test.java
@@ -30,5 +30,66 @@ public class MyInteger_Test {
 	public void test() {
 		assertTrue(1==1);
 	}
+	@Test
+	public void testiEven() {
+		MyInteger i = new MyInteger(4);
+		assertTrue(i.isEven());
+	}
+	
+	@Test
+	public void testisOdd(){
+		MyInteger i = new MyInteger(5);
+		assertTrue(i.isOdd());
+	}
+	
+	@Test
+	public void testisPrime(){
+		MyInteger i = new MyInteger(7);
+		assertTrue(i.isPrime());
+	}
+	
+	@Test
+	public void testsisEven1(){
+		assertTrue(MyInteger.isEven(4));
+	}
+	
+	@Test
+	public void testsisOdd1(){
+		assertTrue(MyInteger.isOdd(3));
+	}
+	
+	@Test
+	public void testsisPrime1(){
+		assertTrue(MyInteger.isPrime(7));
+	}
+	
+	@Test
+	public void testsisEven(){
+		assertTrue(MyInteger.isEven(new MyInteger(4)));
+	}
+	
+	@Test
+	public void testsisOdd(){
+		assertTrue(MyInteger.isOdd(new MyInteger(5)));
+	}
+	
+	@Test
+	public void testsisPrime(){
+		assertTrue(MyInteger.isPrime(new MyInteger(7)));
+	}
+	
+	@Test
+	public void testequals1(){
+		MyInteger a = new MyInteger(4);
+		MyInteger b = new MyInteger(4);
+		assertTrue(a.equals(b));
+	}
+	
+	@Test
+	public void testequals(){
+		MyInteger a = new MyInteger(4);
+		int b = 4;
+		assertTrue(a.equals(b));
+	}
 
 }
