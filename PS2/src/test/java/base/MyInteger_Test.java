47dccba202d0608a357f6a76dc3696e7f8a1be1f
alexianebrao <alexianebrao@wifi-roaming-128-4-82-226.host.udel.edu>
2016-09-23 00:20:30 -0400

    tests

--- PS2/src/test/java/base/MyInteger_Test.java
+++ PS2/src/test/java/base/MyInteger_Test.java
@@ -28,68 +28,87 @@ public class MyInteger_Test {
 
 	@Test
 	public void test() {
-		assertTrue(1==1);
+		assertTrue(1 == 1);
 	}
 	@Test
 	public void testiEven() {
-		MyInteger i = new MyInteger(4);
-		assertTrue(i.isEven());
+		MyInteger a = new MyInteger(4);
+		MyInteger b = new MyInteger(5);
+		MyInteger c = new MyInteger(19);
+		assertTrue(a.isEven());		
+		assertFalse(b.isEven());
+		assertFalse(c.isEven());
 	}
 	
 	@Test
 	public void testisOdd(){
 		MyInteger i = new MyInteger(5);
+		MyInteger b = new MyInteger(8);
 		assertTrue(i.isOdd());
+		assertFalse(b.isOdd());
+		
 	}
 	
 	@Test
 	public void testisPrime(){
 		MyInteger i = new MyInteger(7);
+		MyInteger b = new MyInteger(8);
 		assertTrue(i.isPrime());
+		assertFalse(b.isPrime());
 	}
 	
 	@Test
 	public void testsisEven1(){
 		assertTrue(MyInteger.isEven(4));
+		assertFalse(MyInteger.isEven(3));
 	}
 	
 	@Test
 	public void testsisOdd1(){
 		assertTrue(MyInteger.isOdd(3));
+		assertFalse(MyInteger.isOdd(8));
 	}
 	
 	@Test
 	public void testsisPrime1(){
 		assertTrue(MyInteger.isPrime(7));
+		assertFalse(MyInteger.isPrime(6));
 	}
 	
 	@Test
 	public void testsisEven(){
 		assertTrue(MyInteger.isEven(new MyInteger(4)));
+		assertFalse(MyInteger.isEven(new MyInteger(9)));
 	}
 	
 	@Test
 	public void testsisOdd(){
 		assertTrue(MyInteger.isOdd(new MyInteger(5)));
+		assertFalse(MyInteger.isOdd(new MyInteger(8)));
 	}
 	
 	@Test
 	public void testsisPrime(){
 		assertTrue(MyInteger.isPrime(new MyInteger(7)));
+		assertFalse(MyInteger.isPrime(new MyInteger(8)));
 	}
 	
 	@Test
 	public void testequals1(){
 		MyInteger a = new MyInteger(4);
 		MyInteger b = new MyInteger(4);
+		MyInteger c = new MyInteger(5);
 		assertTrue(a.equals(b));
+		assertFalse(a.equals(c));
 	}
 	
 	@Test
 	public void testequals(){
 		MyInteger a = new MyInteger(4);
-		int b = 4;
-		assertTrue(a.equals(b));
+		MyInteger b = new MyInteger(7);
+		int c = 4;
+		assertTrue(a.equals(c));
+		assertFalse(b.equals(c));
 	}
 
 }
